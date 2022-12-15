3197be01216b39248c45b713bb0a5164342bc876
Murat Yüksel <mymrtyuksel@gmail.com>
2022-12-15 15:36:54 +0300

    fixed multiple fetching bug

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 
 let quotesElement = document.getElementById("quotes");
 let loaderElement = document.getElementById("loader");
-
 let showLoader = () => { loaderElement.style.opacity = 1 };
 let hideLoader = () => { loaderElement.style.opacity = 0 };
 
-async function fetchQuotes() {
 
+async function fetchQuotes() {
     let random = Math.floor(Math.random() * 200);
     //Get a random list of 10 quotes
     let quoteList = await fetch(`https://quotable.io/quotes?page=${random}&limit=10`);
 
-        if(!quoteList.ok) {
-            throw new Error(`Data could not be loaded: ${quoteList.status}`)
-        }
+    if (!quoteList.ok) {
+        throw new Error(`Data could not be loaded: ${quoteList.status}`)
+    }
 
-        quoteList = await quoteList.json();
+    quoteList = await quoteList.json();
 
     //Create quote elements and add them to #quotes div.
     quoteList.results.forEach(quote => {
@@ -30,19 +29,23 @@ async function fetchQuotes() {
     hideLoader();
 }
 
-setTimeout(fetchQuotes,200);
+setTimeout(fetchQuotes, 200);
 
+let shouldFetch = true; // to prevent multiple fetching
 window.addEventListener('scroll', () => {
-    showLoader();
     const {
         scrollTop,
         scrollHeight,
         clientHeight
     } = document.documentElement;
 
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
-       setTimeout(fetchQuotes, 500); //to see the cute loader icon longer :)
+    if (scrollTop + clientHeight >= scrollHeight - 5 && shouldFetch) {
+        shouldFetch = false;
+        showLoader();
+        setTimeout(fetchQuotes, 400); //400, to see the loader icon longer :)
+        setTimeout(() => { shouldFetch = true }, 1000);
     }
+
 }, {
     passive: true
 })
